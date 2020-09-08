bd1abe2322c0f1f867422aa7b0c21d4709d6add6
seth-bradshaw <seth.bradshaw27@gmail.com>
2020-09-08 17:16:56 -0600

    Saving for submission, not done yet.

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
+const votingAge = 19;
+
+if(votingAge >= 18){
+  console.log("true")
+} else {
+  console.log("false")
+}
 
 
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
+let dog ="pitbull";
+let adorable = "Yes";
 
+if(adorable === "Yes"){
+    dog = "Pitbulls are adorable, indeed."
+} else{
+    dog = "You are very wrong and you don't deserve pitbulls."
+}
 
+console.log(dog);
 
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
+let year = "1999";
 
+if(year == 1999){
+    year = parseInt("1999");
+    console.log(year);
+}
 
 
 
 
 //Task d: Write a function to multiply a*b 
+const a = 0;
+const b = 100;
 
+
+function multiply(a, b){
+    return a * b;
+}
+
+console.log(multiply(5,200));
 
 
 
@@ -26,7 +54,13 @@
 /************************************************************** Task 2 **************************************************************/
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
+let yourAge = 20;
 
+function dogYears(yourAge){
+    return yourAge * 7;
+}
+
+console.log(dogYears(yourAge));
 
 
 
@@ -49,7 +83,25 @@
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
   
-
+function dog(age, weight) {
+    if(age >= 1 && weight <= 5) {
+      return weight * 0.05
+    }else if(age >= 1 && weight >= 6 && weight <= 10) {
+      return weight * 0.04
+    }else if(age >= 1 && weight <= 11 && weight < 15) {
+      return weight * 0.03
+    }else if(age >= 1 && weight >= 15) {
+      return weight * 0.02
+    }else if(age < 1 && age >= 0.66) {
+      return weight * 0.04
+    }else if(age < .66 && age >= 0.333) {
+      return weight * 0.05
+    }else if(age < .33 && age >= 0.01) {
+      return weight * 0.1
+    }
+  }
+  
+  console.log(dog(1, 15));
 
 
 
@@ -59,7 +111,15 @@
 // it should return you won or you lost based on the rules of the game (you may need to look up the rules if you have not played before)
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
+let userSelection = prompt("Rock, paper, or sissors?");
 
+
+function game(rock,paper,sissors){
+    number = Math.floor((Math.random() * 3) + 1);
+    if(number === 1){
+
+    }
+}
   
   
 
